ba3b11f31cbb6a04a0ee276329c6012da244ac43
X <a@s>
2024-10-11 12:15:30 +0800

    Add project files.

---
+++ ACT4/StateHeuristic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ACT4
+{
+    public class StateHeuristic
+    {
+        public SixState State { get; set; }
+        public int Heuristic { get; set; }
+
+        public StateHeuristic(SixState state, int heuristic)
+        {
+            State = state;
+            Heuristic = heuristic;
+        }
+    }
+}
